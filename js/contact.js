7b9b01bbcbb4a097702d6d1c9d56b6c3cff90848
Lautaro Zarate <zlautaro43@gmail.com>
2024-09-24 17:53:14 -0300

    Empezando el proyecto final

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+// localStorage.clear();
+let formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", confirmacionDeRegistro) 
+
+function confirmacionDeRegistro(e){
+    e.preventDefault();
+    let nombreUsuario = document.getElementById("nombre").value;
+    let apellidoUsuario = document.getElementById("apellido").value;
+    let emailUsuario = document.getElementById("email").value;
+    let contraseñaUsuario = document.getElementById("password").value;
+
+    const nuevoUsuario = {
+        nombreUsuario : nombreUsuario,
+        apellidoUsuario : apellidoUsuario,
+        emailUsuario : emailUsuario,
+        contraseñaUsuario : contraseñaUsuario
+    }
+    
+    localStorage.setItem("registroDeUsuario", JSON.stringify(nuevoUsuario));
+    
+    Swal.fire({
+        position: "top-center",
+        icon: "success",
+        text: "Has sido registrado correctamente",
+        color: "#fff",
+        background:"#3a4856",
+        showConfirmButton: false,
+        timer: 2500
+    });
+    
+    formulario.reset()
+}
+
+
+
---
+++ js/script.js
@@ -0,0 +1,18 @@
+const section = document.getElementById("section")
+let apiLocal = fetch(`./js/object.json`)
+.then(resp => resp.json())
+.then(data => {
+    // console.log(data)
+    data.forEach(prod => {
+        const li = document.createElement("div")
+        li.innerHTML = `
+            <div class="flex flex-col bg-white p-2 shadow-xl items-stretch">
+                <img src="${prod.imagen}" class="card__image" alt="Zapatillas Nike">
+                <h3 class="card__text">${prod.nombre}</h3>
+                <div class="card__container">
+                    <p class="card__precio" >${prod.precio}</p>
+                    <input type="button" value="Añadir al Carrito" class= "card__carrito bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-900 hover:scale-105 transition-all duration-300 ease-in-out">
+                </div>`
+        section.append(li)
+    });
+})
