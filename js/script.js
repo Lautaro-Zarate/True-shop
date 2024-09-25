4d83b6516ebf14aeb0d0b6891792300f94000a76
Lautaro Zarate <zlautaro43@gmail.com>
2024-09-25 16:11:06 -0300

    Agregando filtrado de productos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const section = document.getElementById("section")
 let apiLocal = fetch(`./js/object.json`)
-.then(resp => resp.json())
-.then(data => {
+.then((resp) => resp.json())
+.then((data) => {
     // console.log(data)
-    data.forEach(prod => {
+    data.forEach((prod) => {
         const li = document.createElement("div")
         li.innerHTML = `
             <div class="flex flex-col bg-white p-2 shadow-xl items-stretch">
@@ -11,8 +11,27 @@ let apiLocal = fetch(`./js/object.json`)
                 <h3 class="card__text">${prod.nombre}</h3>
                 <div class="card__container">
                     <p class="card__precio" >$${prod.precio}</p>
-                    <input type="button" value="Añadir al Carrito" class= "card__carrito bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-900 hover:scale-105 transition-all duration-300 ease-in-out">
+                    <input type="button" value="Añadir al Carrito" class= "card__carrito  bg-blue-500 text-white font-bold py-2 px-4 rounded-lg active:bg-blue-900 active:scale-105 transition-all duration-150 ease-in-out" id="${prod.id}">
                 </div>`
         section.append(li)
     });
 })
+
+let productos = []; 
+fetch(`./js/object.json`)
+.then((resp) => resp.json())
+.then((data) => {
+    productos = data
+    filtradoDeZapatillas()
+})
+.catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+function filtradoDeZapatillas(){
+    const resultado = productos.filter((el) => el.nombre.includes("Zapatillas"))
+    console.log(resultado)
+}
+
+
+// console.log(apiLocal)
+// const camperas = document.getElementById("camperas")
+// camperas.addEventListener("click", filtradoDeCamperas)
