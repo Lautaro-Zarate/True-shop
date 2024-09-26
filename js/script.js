2804f93fc5a565f5e0ba6ae5058a06fea3380fa3
Lautaro Zarate <zlautaro43@gmail.com>
2024-09-25 21:29:51 -0300

    Agregado de filtrado de prendas

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ let apiLocal = fetch(`./js/object.json`)
                 <div class="card__container">
                     <p class="card__precio" >$${prod.precio}</p>
                     <input type="button" value="Añadir al Carrito" class= "card__carrito  bg-blue-500 text-white font-bold py-2 px-4 rounded-lg active:bg-blue-900 active:scale-105 transition-all duration-150 ease-in-out" id="${prod.id}">
-                </div>`
+                </div>
+            </div>`
         section.append(li)
     });
 })
@@ -21,17 +22,49 @@ let productos = [];
 fetch(`./js/object.json`)
 .then((resp) => resp.json())
 .then((data) => {
-    productos = data
-    filtradoDeZapatillas()
+    productos = data;
 })
 .catch(error => console.error('Error al cargar el archivo JSON:', error));
 
-function filtradoDeZapatillas(){
-    const resultado = productos.filter((el) => el.nombre.includes("Zapatillas"))
-    console.log(resultado)
+function filtrarCategoria(categoria) {
+    const productosFiltrados = productos.filter(producto => producto.categoria === categoria);
+    mostrarProductos(productosFiltrados);
+}
+function mostrarProductos(filtrados) {
+    const contenedor = document.getElementById('section');
+    contenedor.innerHTML = "";
+    filtrados.forEach(prenda => {
+        const item = document.createElement('div');
+        item.innerHTML = `
+            <div class="flex flex-col bg-white p-2 shadow-xl items-stretch">
+                <img src="${prenda.imagen}" class="card__image" alt="Zapatillas Nike">
+                <h3 class="card__text">${prenda.nombre}</h3>
+                <div class="card__container">
+                    <p class="card__precio" >$${prenda.precio}</p>
+                    <input type="button" value="Añadir al Carrito" class= "card__carrito  bg-blue-500 text-white font-bold py-2 px-4 rounded-lg active:bg-blue-900 active:scale-105 transition-all duration-150 ease-in-out" id="${prenda.id}">
+                </div>
+            </div>`;
+        contenedor.appendChild(item);
+    });
 }
 
-
-// console.log(apiLocal)
-// const camperas = document.getElementById("camperas")
-// camperas.addEventListener("click", filtradoDeCamperas)
+const camperas = document.getElementById("camperas")
+camperas.addEventListener("click", function(e) {
+    e.preventDefault();
+    filtrarCategoria("camperas")
+})
+const remeras = document.getElementById("remeras")
+remeras.addEventListener("click", function(e) {
+    e.preventDefault();
+    filtrarCategoria("remeras")
+})
+const pantalones = document.getElementById("pantalones")
+pantalones.addEventListener("click", function(e) {
+    e.preventDefault();
+    filtrarCategoria("pantalones")
+})
+const zapatillas = document.getElementById("zapatillas")
+zapatillas.addEventListener("click", function(e) {
+    e.preventDefault();
+    filtrarCategoria("zapatillas")
+})
